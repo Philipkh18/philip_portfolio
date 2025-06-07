8f1442b3a40c23163e774c0cc4b5b5a07ed2a495
Philip Hadiwidjaja <Philipkh@umich.edu>
2025-06-06 21:21:04 -0400

    changed filepaths

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -12,34 +12,34 @@ export default function Projects() {
       <div className="project-display">
         <ProjectCard
           title="Tech Layoff Predictor"
-          image="public/eecs398_proj.png"
+          image="/eecs398_proj.png"
           description="Predictive model using pandas and scikit-learn to estimate tech layoff percentages based on company characteristics and geographic indicators"
         />
         <ProjectCard
           title="Medical Scanner"
-          image="public/bloopsProject.png"
+          image="/bloopsProject.png"
           description="An AI-powered medical scanner app that reduces human error and automates inventory tracking using GPT-4 and Gemini API."
         />
         <ProjectCard
           title="Iota Omega Epsilon"
-          image="public/IOEWebsite.png"
+          image="/IOEWebsite.png"
           description="Developed a responsive React.js website for Iota Omega Epsilon to showcase its mission, events, and brotherhood with a modern and accessible design."
         />
         <ProjectCard
           title="Portfolio website"
-          image="public/portfolio.png"
+          image="/portfolio.png"
           description="Developed a responsive portfolio website with react and javascipt as well as Material UI"
         />
 
         <ProjectCard
           title="Slime run"
-          image="public/SlimeRun.png"
+          image="/SlimeRun.png"
           description="Mario-styled side scroll game with created map made with Java and Libgdx"
         />
 
         <ProjectCard
           title="Stock Simulator"
-          image="public/stockSimulator.png"
+          image="/stockSimulator.png"
           description="Engineered a real-time stock market simulator in C++ to model price movements and identify optimal buy-in points based on historical trends and dynamic thresholds leading to a guaranteed profit."
         />
       </div>
